6faa56087591754c0ae9fef94053813317b424af
SiNiquity <20628674+SiNiquity@users.noreply.github.com>
2024-01-04 10:50:07 -0800

    Add keybind to open Cargo.toml

--- nvim/after/plugin/rust-tools.lua
+++ nvim/after/plugin/rust-tools.lua
@@ -12,6 +12,8 @@ rt.setup({
             vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
             -- Code action groups
             vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+            -- Open Cargo.toml file
+            vim.keymap.set("n", "<Leader>c", rt.open_cargo_toml.open_cargo_toml, { buffer = bufnr })
         end,
     },
 })
