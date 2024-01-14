332224657a3327986c07d53dcdc10047b4a8c722
SiNiquity <20628674+SiNiquity@users.noreply.github.com>
2024-01-14 10:26:46 -0800

    Added missing vsnip plugin

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -7,6 +7,7 @@ return {
              'hrsh7th/cmp-nvim-lsp',
              'hrsh7th/cmp-buffer',
              'hrsh7th/cmp-vsnip',
+             'hrsh7th/vim-vsnip',
              'hrsh7th/cmp-nvim-lua',
              'hrsh7th/cmp-nvim-lsp-signature-help',
              'hrsh7th/cmp-path',
